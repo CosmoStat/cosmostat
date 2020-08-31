7afdadd46e9de20e3e491ada53f37e81421a32fc
Samuel Farrens <samuel.farrens@gmail.com>
2020-08-31 13:08:25 +0200

    save status

--- src/cxx/astro/weaklensing/libwl/shearinversions.h
+++ src/cxx/astro/weaklensing/libwl/shearinversions.h
@@ -44,28 +44,28 @@
 
 class ShearInversions
 {
-  
+
 public:
   ShearInversions(int NZeroPadded);
   ~ShearInversions();
-  
+
   void gamma2kappa(dblarray &gamma1, dblarray &gamma2, dblarray &kappa);
   void gamma2kappa(double *gamma1, double *gamma2, double *kappa, int Nx, int Ny, int Nz);
-  
+
   void flexion2kappa(double pixel_size, dblarray &F1, dblarray &F2, dblarray &kappa);
   void flexion2kappa(double pixel_size, double *F1, double *F2, double *kappa, int Nx, int Ny, int Nz);
-  
+
   void kappa2gamma(dblarray &kappa, dblarray &gamma1, dblarray &gamma2);
   void kappa2gamma(double *kappa,double *gamma1, double *gamma2,  int Nx, int Ny, int Nz);
-  
+
   void kappa2flexion(double pixel_size, dblarray &kappa, dblarray &F1, dblarray &F2);
   void kappa2flexion(double pixel_size, double *kappa,double *F1, double *F2,  int Nx, int Ny, int Nz);
-  
+
   void gamma_flexion2kappa(double pixel_size, dblarray &gamma1, dblarray &gamma2, dblarray &F1, dblarray &F2, dblarray &kappa);
   void gamma_flexion2kappa(double pixel_size, double sigma_g, double sigma_f, double* gamma1, double* gamma2, double* F1, double* F2, double* kappa, int Nx, int Ny, int Nz);
-  
+
 private:
-  
+
   int NpixFFT;
   double fftFactor;
   fftw_complex* fftFrame1;
@@ -73,7 +73,7 @@ private:
   fftw_complex* fftFrame3;
   fftw_complex* fftFrame4;
   fftw_plan plan_forward1, plan_backward1,plan_forward2, plan_backward2,plan_forward3,plan_forward4;
-  
+
 };
 
 #endif // SHEARINVERSIONS_H
--- src/cxx/modules/BuildSparse2D.cmake
+++ src/cxx/modules/BuildSparse2D.cmake
@@ -5,13 +5,24 @@
 # Set Sparse2D Version
 set(Sparse2DVersion v2.1.5_beta)
 
-message(STATUS ${SPARSE2D_SOURCE})
+message(STATUS "SPARSE2D_SOURCE: ${SPARSE2D_SOURCE}")
 
 # Download and build Sparse2D
 ExternalProject_Add(sparse2d-git
   GIT_REPOSITORY https://github.com/sfarrens/Sparse2D.git
   GIT_TAG ${Sparse2DVersion}
   PREFIX sparse2d
-  CMAKE_ARGS ${CMAKE_ARGS} -DBUILD_MSVST=ON -DUSE_FFTW=OFF -DBUILD_NFFT=OFF
+  CMAKE_ARGS ${CMAKE_ARGS} -DBUILD_MSVST=ON -DUSE_FFTW=ON -DBUILD_NFFT=OFF
   # INSTALL_COMMAND make install
 )
+
+set(FFTW_LD_FLAGS "-L ${SPARSE2D_SOURCE}/sparse2d-git-build/module_build/fftw/lib -lfftw3f_omp -lfftw3_omp \
+-lfftw3f -lfftw3 -lm")
+
+set(sparse2d_lib_names mga2d sparse3d sparse2d sparse1d tools)
+set(sparse2d_libs "")
+foreach(library ${sparse2d_lib_names})
+  include_directories(${SPARSE2D_SOURCE}/sparse2d-git/src/lib${library})
+  find_library(${library} NAMES ${library} PATHS ${SPARSE2D_SOURCE}/sparse2d-git-build)
+  list(APPEND sparse2d_libs ${${library}})
+endforeach()
