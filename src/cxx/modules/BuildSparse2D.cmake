514b77e266e0da389b416d461b13cfc2214d89d4
Samuel Farrens <samuel.farrens@gmail.com>
2020-09-11 13:38:18 +0200

    updated Sparse2D version

--- src/cxx/modules/BuildSparse2D.cmake
+++ src/cxx/modules/BuildSparse2D.cmake
@@ -3,11 +3,12 @@
 #=================#
 
 # Set Sparse2D Version
-set(Sparse2DVersion v2.1.5_beta3)
+set(Sparse2DVersion 9aafc9b)
 
 # Set Sparse2D source dir
 set(SPARSE2D_SOURCE ${CMAKE_CURRENT_BINARY_DIR}/sparse2d)
 
+# Check if FFTW should be built
 option(BUILD_FFTW "Build FFTW libraries" OFF)
 if(NOT FFTW3_FOUND)
   set(BUILD_FFTW ON)
@@ -18,34 +19,34 @@ ExternalProject_Add(sparse2d-git
   GIT_REPOSITORY https://github.com/sfarrens/Sparse2D.git
   GIT_TAG ${Sparse2DVersion}
   PREFIX sparse2d
-  CMAKE_ARGS ${CMAKE_ARGS} -DBUILD_MSVST=ON -DUSE_FFTW=ON -DBUILD_FFTW=${BUILD_FFTW} -DBUILD_NFFT=OFF
+  CMAKE_ARGS ${CMAKE_ARGS} -DBUILD_MSVST=ON -DUSE_FFTW=ON
+-DBUILD_FFTW=${BUILD_FFTW} -DBUILD_NFFT=OFF
 -DCMAKE_INSTALL_PREFIX:PATH=${CMAKE_INSTALL_PREFIX}
 )
 
 # List Sparse2D libs
 set(sparse2d_libs mga2d sparse3d sparse2d sparse1d tools)
 
-# Include external headers
-include_directories(${CMAKE_INSTALL_PREFIX}/include/sparse2d)
-
 # List FFTW libs
 set(fftw_libs fftw3f_omp fftw3_omp fftw3f fftw3)
 
+# Include external headers
+include_directories(${CMAKE_INSTALL_PREFIX}/include/sparse2d)
+
+# Extract FFTW libraries from Sparse2D build
 if(NOT FFTW3_FOUND)
-  # Extract FFTW libraries from Sparse2D build
   ExternalProject_Add_Step(sparse2d-git fftw
     DEPENDEES install
     COMMAND cp -r ${SPARSE2D_SOURCE}/src/sparse2d-git-build/module_build/lib/. ${CMAKE_INSTALL_PREFIX}/lib
     COMMAND cp -r ${SPARSE2D_SOURCE}/src/sparse2d-git-build/module_build/include/. ${CMAKE_INSTALL_PREFIX}/include/sparse2d
     COMMENT "Extracting FFTW build"
   )
-  # Add external libraries
   set(external_libs ${sparse2d_libs} ${fftw_libs})
 else()
-  # Add external libraries
   set(external_libs ${sparse2d_libs})
 endif()
 
+# Add external libraries
 foreach(library ${external_libs})
   add_library(${library} STATIC IMPORTED)
   set_property(TARGET ${library} PROPERTY IMPORTED_LOCATION ${CMAKE_INSTALL_PREFIX}/lib/lib${library}.a)
